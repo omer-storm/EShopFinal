ebb2a897d0df911f8b77554c7036f198cb28c2d8
omer-storm <omerpk.os@gmail.com>
2023-01-29 20:30:11 +0500

    first commit

---
+++ EShopFinal/Controllers/Adminpanel/AdminpanelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShopFinal.Controllers.Adminpanel
+{
+    public class AdminpanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
